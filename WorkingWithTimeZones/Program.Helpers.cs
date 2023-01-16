53f9c5195520610a61f3ad8f2041caafdea7a54a
Kenneth Hendricks <kennethhendricks10@gmail.com>
2023-01-16 09:51:17 +0000

    Working with time

---
+++ WorkingWithTimeZones/Program.Helpers.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.ObjectModel;
+
+partial class Program
+{
+    static void SectionTitle(string title)
+    {
+        ConsoleColor previousColor = ForegroundColor;
+        ForegroundColor = ConsoleColor.DarkYellow;
+        WriteLine("*");
+        WriteLine($"* {title}");
+        WriteLine("*");
+        ForegroundColor = previousColor;
+    }
+
+    static void OutputTimeZones()
+    {
+        ReadOnlyCollection<TimeZoneInfo> zones =
+            TimeZoneInfo.GetSystemTimeZones();
+        WriteLine("*");
+        WriteLine($"* {zones.Count} time zones");
+        WriteLine("*");
+
+        foreach (TimeZoneInfo zone in zones.OrderBy(z => z.Id))
+        {
+            WriteLine($"{zone.Id}");
+        }
+    }
+
+    static void OutputDateTime(DateTime dateTime, string title) 
+    {
+        SectionTitle(title);
+        WriteLine($"Value: {dateTime}");
+        WriteLine($"Kind: {dateTime.Kind}");
+        WriteLine($"IsDayLightSavingTime: {dateTime.IsDaylightSavingTime()}");
+        WriteLine($"ToLocalTime(): {dateTime.ToLongDateString()}");
+        WriteLine($"ToUniversalTime(): {dateTime.ToUniversalTime()}");
+    }
+
+    static void OutputTimeZone(TimeZoneInfo zone, string title)
+    {
+        SectionTitle(title);
+        WriteLine($"Id: {zone.Id}");
+        WriteLine("IsDaylightSavingTime(DateTime.Now): {0}", 
+            zone.IsDaylightSavingTime(DateTime.Now));
+        WriteLine($"StandardName: {zone.StandardName}");
+        WriteLine($"DaylightName: {zone.DisplayName}");
+        WriteLine($"BaseUtcOffset: {zone.BaseUtcOffset}");
+    }
+
+    static string GetCurrentZoneName(TimeZoneInfo zone, DateTime when)
+    {
+        return zone.IsDaylightSavingTime(when) 
+            ? zone.DaylightName 
+            : zone.StandardName;
+    }
+}
+
